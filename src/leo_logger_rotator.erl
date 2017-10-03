aa5bdf74436ba6c4a701b7eeeceebd162c35bd62
Wilson Li <windkithk@gmail.com>
2017-10-03 18:00:41 +0900

    Added lager_logger_rotator behaviour

--- src/leo_logger_rotator.erl
+++ src/leo_logger_rotator.erl
@@ -39,6 +39,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
+-behaviour(lager_rotator_behaviour).
 %% Lager rotator callbacks
 -export([create_logfile/2, open_logfile/2,
          ensure_logfile/4, rotate_logfile/2]).
